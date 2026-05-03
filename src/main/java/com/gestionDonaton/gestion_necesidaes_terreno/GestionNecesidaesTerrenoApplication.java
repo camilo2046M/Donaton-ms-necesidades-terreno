8d03e473e4e603097f22e5a818c849bd0d27831b
camilo2046M <ca.menav@duocuc.cl>
2026-05-03 12:54:01 -0400

    Initial commit: Inicialización de configuración base de pom.xml para el microservicio de necesidades en el terreno

---
+++ src/main/java/com/gestionDonaton/gestion_necesidaes_terreno/GestionNecesidaesTerrenoApplication.java
@@ -0,0 +1,13 @@
+package com.gestionDonaton.gestion_necesidaes_terreno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionNecesidaesTerrenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionNecesidaesTerrenoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionDonaton/gestion_necesidaes_terreno/GestionNecesidaesTerrenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionDonaton.gestion_necesidaes_terreno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionNecesidaesTerrenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
